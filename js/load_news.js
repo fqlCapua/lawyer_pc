3cc2dea8ef94230af23fe2ebcf117e5609784baf
WebArenaCN <13523450460@sina.cn>
2017-07-25 17:49:43 +0800

    更新新闻中心

--- js/load_news.js
+++ js/load_news.js
@@ -1,5 +1,4 @@
 function send_to_export(tag,page){
-	
 	var cur_timestamp = Date.parse(new Date()) / 1000;
     var md_token = hex_md5("law_" + hex_md5(String(cur_timestamp)) + "_law");
 	var index = layer.load(2, {
@@ -21,7 +20,11 @@ function send_to_export(tag,page){
 		   layer.close(index);
 		   var data=JSON.parse(data);;
 		   if(data.ret == 200) {
-				console.log(data.data);
+				var newsList=data.data;
+				$.each(newsList, function(i,ele){
+				        var news=$("<div news_tag='"+ele.news_tag+"' news_views='"+ele.news_views+"' news_id='"+ele.news_id+"' class='media'><a class='pull-left' href='#'><img style='width:200px;height:100px;margin:5px;' class='media-object' src='"+ele.news_small_thumb+"' alt='Media Object'></a><div class='media-body'><h4 class='media-heading'>"+ele.news_title+"</h4><div class='media-desc'>"+ele.news_desc+"</div><div class='news_ctime text-muted pull-right news_ctime'>"+new Date(parseInt(ele.news_ctime) * 1000).toLocaleString().split(" ")[0]+"</div></div></div>");
+					$('#home').append(news);
+				});
 			} else {
 				layer.msg(data.msg);
 			}
